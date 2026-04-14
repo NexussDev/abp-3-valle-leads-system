8b0f1d30d0e2120842717aca4fd2449380f8451c
teodoroooo <gabriel.rteodoro@yahoo.com.br>
2026-04-14 11:03:48 -0300

    feat: implementação de autenticação JWT e RBAC >> - atualização authMiddleware >> - proteção de rotas

--- backend/src/presentation/routes/userRoutes.ts
+++ backend/src/presentation/routes/userRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import userController from '../controllers/UserController';
+import { authMiddleware } from '../../infrastructure/middleware/authMiddleware';
+import { roleMiddleware } from '../../infrastructure/middleware/roleMiddleware';
 
 const router = Router();
 
-router.get('/', userController.index);
-router.get('/:id', userController.show);
-router.post('/', userController.store);
-router.put('/:id', userController.update);
-router.delete('/:id', userController.destroy);
+router.get('/', authMiddleware, roleMiddleware('ADMIN'), userController.index);
+router.get('/:id', authMiddleware, roleMiddleware('ADMIN'), userController.show);
+router.post('/', authMiddleware, roleMiddleware('ADMIN'), userController.store);
+router.put('/:id', authMiddleware, roleMiddleware('ADMIN'), userController.update);
+router.delete('/:id', authMiddleware, roleMiddleware('ADMIN'), userController.destroy);
 
 export default router;
